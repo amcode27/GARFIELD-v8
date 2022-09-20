16f3a2cb759bb9eb07ac6a6a6a9c96d7e0c6b86c
amcode27 <113937687+amcode27@users.noreply.github.com>
2022-09-20 13:54:39 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -13,52 +13,47 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 // 👈 You Can change this your choice
-global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
-global.emoji02 = ['🦋'] //👈 Enter your Emoji
-global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
-*I am a Learning.*
-*Not more..*
-*Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
-global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
-global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
-global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
-global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
-global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
-global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
-global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
-global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
+global.emoji01 = ['🔮'] // 👈 Enter your Emoji 
+global.emoji02 = ['🪀'] //👈 Enter your Emoji
+global.notice = ` 
+*Hi 🥰 I m*
+*◥དAdheBaworཌ◤*`  // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+6282138037447'] //👈  Enter Your number
+global.premium =  ['+6285211112701'] //👈  Enter Your number
+global.ownernomer = '+6282138037447' //👈  Enter Your number
+global.ownername = '🪀◥དAdheBaworཌ◤' //👈 Enter Your name
+global.botname = '꧁ঔৣ☬AMͥcͣoͫde27☬ঔৣ꧂' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 ◥དAdheBaworཌ◤' //👈 Enter Your Name
+global.ig = 'https://youtube.com/c/JasBiruLangit?sub_confirmation=1' // 👈 You Can change this your choice
+global.region = 'Cilacap, Jawa Tengah, Indonesia' // 👈 You Can change this your choice
+global.sc = 'https://bit.ly/CekDISINIIII'
+global.fbt = 'Klik Aja' // 👈 Enter (Subscribe/Follow/Like now)
+global.myweb = 'https://www.amcode27@.my.id/'// 👈 Enter your Social media link to follow now button
+global.welcome = '*🪀 Selamat Datang Di Grup.*' // 👈 You Can change this your choice
+global.bye = '*Selamat Tinggal Broo. Semoga Tenang Dialam Sana*' // 👈 You Can change this your choice
+global.packname = '꧁ঔৣ☬AMͥcͣoͫde27☬ঔৣ꧂'  // 👈 You Can change this your choice 
+global.author = '🪀◥དAdheBaworཌ◤' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
-global.thumb =  'https://i.ibb.co/Jz0mMnC/Pics-Art-22-06-03-01-01-46-876.png' 
+global.thumb =  'https://telegra.ph/file/8f24dd81957b7f5b990cb.jpg' 
 global.spoty = 'https://i.postimg.cc/HW2YbN1b/Pics-Art-22-08-09-16-12-14-489.png'
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
 global.sp = '⭔'
 global.mess = {
-    success: 'Done 🦋', // 👈 You Can change this your choice
-    admin: 'This Feature Is Only For Admin!', // 👈 You Can change this your choice
-    botAdmin: 'Bot Must Be Admin First!', // 👈 You Can change this your choice
-    owner: 'This Feature Is Only For Owner!', // 👈 You Can change this your choice
-    group: 'Feature Used Only For Groups!', // 👈 You Can change this your choice
-    private: 'Features Used Only For Private Chat!', // 👈 You Can change this your choice
-    bot: 'This Feature Is Only For Bot', // 👈 You Can change this your choice
-    wait: '```Please Wait...```', // 👈 You Can change this your choice
-    error: 'Error! Maybe Api Key Is Expired🤔!', // 👈 You Can change this your choice
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    success: '🪀 Selesai. . .', // 👈 You Can change this your choice
+    admin: 'Fitur ini Hanya untuk Admin!', // 👈 You Can change this your choice
+    botAdmin: 'Fitur ini Harus Admin Dulu!', // 👈 You Can change this your choice
+    owner: 'Fitur Ini Hanya Untuk Pemilik/Owner!', // 👈 You Can change this your choice
+    group: 'Fitur Ini Berjalan di Grup!', // 👈 You Can change this your choice
+    private: 'Fitur Hanya Digunakan Untuk Obrolan Pribadi!', // 👈 You Can change this your choice
+    bot: 'Fitur Ini Hanya Untuk Bot', // 👈 You Can change this your choice
+    wait: '```Tunggu Broo...```', // 👈 You Can change this your choice
+    error: 'Kesalahan! Mungkin Api Key Sudah Kadaluarsa🤔!', // 👈 You Can change this your choice
+    endLimit: 'Limit/Batas Harian Anda Telah Habis, Batas Akan Direset Setiap 12 Jam',
 } // 👈 You Can change this your choice
 /// Do Not Change 👇 
-global.limitAwal = "99999999999999999"
+global.limitAwal = "25"
 global.lolkey = "835641f3d2d1b6971e1fd132"
 global.xteam = "apivproject"
 global.xcodeapi = "5S3epf7hC2"
